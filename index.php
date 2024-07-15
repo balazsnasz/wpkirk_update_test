f113264d960cbb218cd5f38b24232bf30fecd71c
Balazs Nasz <788569+balazsnasz@users.noreply.github.com>
2024-07-16 00:26:24 +0200

    Get plugin version

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name: Balazs Kirk Update Test
  * Plugin URI: https://github.com/wpbones/BalazsKirkUpdate
  * Description: WP Bones template WordPress plugin
- * Version: 3.4
+ * Version: 3.6
  * Author: Giovambattista Fazioli
  * Author URI: http://undolog.com
  * Text Domain: balazs-kirk-update-test
--- plugin/updated.php
+++ plugin/updated.php
@@ -9,11 +9,13 @@
 |
 */
 
-$plugin_data = get_plugin_data($hook_extra['plugin']);
+// Array ( [source] => D:/laragon/www/wordpress/wp-content/upgrade/wpkirk_update_test/ [source_files] => Array ( [0] => api [1] => bootstrap [2] => config [3] => database [4] => functions.php [5] => index.php [6] => LICENSE [7] => localization [8] => pages [9] => plugin [10] => pnpm-lock.yaml [11] => public [12] => readme.txt [13] => release.sh [14] => resources [15] => vendor [16] => YourPackage ) [destination] => D:\laragon\www\wordpress/wp-content/plugins/wpkirk_update_test/ [destination_name] => wpkirk_update_test [local_destination] => D:\laragon\www\wordpress/wp-content/plugins [remote_destination] => D:/laragon/www/wordpress/wp-content/plugins/wpkirk_update_test/ [clear_destination] => 1 )
+
+$plugin_data = get_plugin_data($result['local_destination'] . '/' . $hook_extra['plugin']);
 $plugin_version = $plugin_data['Version'];
 
-//$plugin_dir = plugin_dir_path($hook_extra['plugin']);
-$updatedFile = fopen("updated.txt", "a") or die("Unable to open file!");
+$plugin_dir = $result['destination'];
+$updatedFile = fopen($plugin_dir . "/updated.txt", "a") or die("Unable to open file!");
 
 $updatedTxt = "Date: ";
 $updatedTxt .= date("Y-m-d H:i:s");
