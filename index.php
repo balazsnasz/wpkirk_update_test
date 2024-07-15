a437a0330e67873ebe33f31e77fad0971f043188
Balazs Nasz <788569+balazsnasz@users.noreply.github.com>
2024-07-15 23:18:10 +0200

    Removed error_log and print_r

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name: Balazs Kirk Update Test
  * Plugin URI: https://github.com/wpbones/BalazsKirkUpdate
  * Description: WP Bones template WordPress plugin
- * Version: 2.7
+ * Version: 2.8
  * Author: Giovambattista Fazioli
  * Author URI: http://undolog.com
  * Text Domain: balazs-kirk-update-test
