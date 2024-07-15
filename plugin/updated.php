29422c48e37c1870af43ba195edb8fe3339fc538
Balazs Nasz <788569+balazsnasz@users.noreply.github.com>
2024-07-15 23:41:33 +0200

    Update plugin_version data in updated.php

--- plugin/updated.php
+++ plugin/updated.php
@@ -9,13 +9,16 @@
 |
 */
 
+$plugin_data = get_plugin_data( plugin_basename(__FILE__) );
+$plugin_version = $plugin_data['Version'];
+
 //$plugin_dir = WP_PLUGIN_DIR . '/wpkirk_update_test';
 $updatedFile = fopen("updated.txt", "a") or die("Unable to open file!");
 
 $updatedTxt = "Date: ";
 $updatedTxt .= date("Y-m-d H:i:s");
 $updatedTxt .= " - Version: ";
-$updatedTxt .= $plugin->Version;
+$updatedTxt .= $plugin_version;
 $updatedTxt .= "\n";
 
 fwrite($updatedFile, $updatedTxt);
--- vendor/wpbones/wpbones/src/Foundation/Plugin.php
+++ vendor/wpbones/wpbones/src/Foundation/Plugin.php
@@ -454,8 +454,6 @@ class Plugin extends Container implements PluginContract
       // Check if the action is an update for a plugin
       if (/*$hook_extra['action'] == 'update' && */isset($hook_extra['plugin'])) {
         // Verify if the updated plugin is the specific one
-        print_r($hook_extra['plugin']);
-        print_r(plugin_basename($this->file));
         if ($hook_extra['plugin'] == plugin_basename($this->file)) {
           // Call the update function
           // include your own activation
