b359a20de5e3c03672ed5ea2634fa006dc91680d
Balazs Nasz <788569+balazsnasz@users.noreply.github.com>
2024-07-15 21:49:10 +0200

    Add updated.php testing

--- plugin/updated.php
+++ plugin/updated.php
@@ -8,3 +8,14 @@
 | This file is included when the plugin is updated.
 |
 */
+
+$updatedFile = fopen("updated.txt", "a") or die("Unable to open file!");
+
+$updatedTxt = "Date: ";
+$updatedTxt .= date("Y-m-d H:i:s");
+$updatedTxt .= " - Version: ";
+$updatedTxt .= $plugin->Version;
+$updatedTxt .= "\n";
+
+fwrite($updatedFile, $updatedTxt);
+fclose($updatedFile);
