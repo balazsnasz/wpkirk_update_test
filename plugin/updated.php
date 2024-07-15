b1489ccbca5ef3e7a29ad8f1bdb9b0895d419cb9
Balazs Nasz <788569+balazsnasz@users.noreply.github.com>
2024-07-15 22:22:01 +0200

    Update updated.php

--- plugin/updated.php
+++ plugin/updated.php
@@ -9,7 +9,8 @@
 |
 */
 
-$updatedFile = fopen("updated.txt", "a") or die("Unable to open file!");
+$plugin_dir = WP_PLUGIN_DIR . '/wpkirk_update_test';
+$updatedFile = fopen($plugin_dir . "/updated.txt", "a") or die("Unable to open file!");
 
 $updatedTxt = "Date: ";
 $updatedTxt .= date("Y-m-d H:i:s");
@@ -19,3 +20,5 @@ $updatedTxt .= "\n";
 
 fwrite($updatedFile, $updatedTxt);
 fclose($updatedFile);
+
+error_log('Kirk updated');
