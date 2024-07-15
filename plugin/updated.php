5c372f15895bd7965bcba7b796d0a31c77cab7c9
Balazs Nasz <788569+balazsnasz@users.noreply.github.com>
2024-07-15 23:51:59 +0200

    Fix plugin data & directory path

--- plugin/updated.php
+++ plugin/updated.php
@@ -9,11 +9,11 @@
 |
 */
 
-$plugin_data = get_plugin_data( plugin_basename(__FILE__) );
+$plugin_data = get_plugin_data($hook_extra['plugin']);
 $plugin_version = $plugin_data['Version'];
 
-//$plugin_dir = WP_PLUGIN_DIR . '/wpkirk_update_test';
-$updatedFile = fopen("updated.txt", "a") or die("Unable to open file!");
+$plugin_dir = plugin_dir_path($hook_extra['plugin']);
+$updatedFile = fopen($plugin_dir . "/updated.txt", "a") or die("Unable to open file!");
 
 $updatedTxt = "Date: ";
 $updatedTxt .= date("Y-m-d H:i:s");
