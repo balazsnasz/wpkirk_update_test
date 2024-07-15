73225ca9a66cc2df873afbc7456c0d584eb47cb8
Balazs Nasz <788569+balazsnasz@users.noreply.github.com>
2024-07-15 22:22:15 +0200

    Bump version

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name: Balazs Kirk Update Test
  * Plugin URI: https://github.com/wpbones/BalazsKirkUpdate
  * Description: WP Bones template WordPress plugin
- * Version: 2.0
+ * Version: 2.1
  * Author: Giovambattista Fazioli
  * Author URI: http://undolog.com
  * Text Domain: balazs-kirk-update-test
